24287a5b8349be6d48a2d7ecc343ac6f65e0ea7e
phpontes <ppontes95@hotmail.com>
2025-02-19 08:52:26 -0300

    Interacting with Host Elements via @HostListener & @HostBinding

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -8,9 +8,18 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
-  }
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; // for backward compatibility
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
   label = input.required<string>();
+
+  onClick() {
+    console.log('Clicked!');
+  }
 }
