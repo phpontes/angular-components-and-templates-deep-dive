3e06971bc42ba11c716be5ed9ae1dc8adf882b1d
phpontes <ppontes95@hotmail.com>
2025-08-12 11:51:47 -0300

    using dashboard-item component

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
 }
