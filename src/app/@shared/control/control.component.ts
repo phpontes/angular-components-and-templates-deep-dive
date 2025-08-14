4ece89786f2e753b48dcf39dd297641e7a5ac59c
phpontes <ppontes95@hotmail.com>
2025-08-14 09:52:15 -0300

    accessing host elements programmatically

--- src/app/@shared/control/control.component.ts
+++ src/app/@shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -13,8 +13,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
 })
 export class ControlComponent {
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   onClick() {
     console.log('Clicked!');
+    console.log(this.el);
   }
 }
